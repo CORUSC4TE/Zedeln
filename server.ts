c93bbb95c0e41f35f60287d127c9a7bc74aa9549
Noah Stewards <noah.stuertz@gmail.com>
2019-11-24 02:42:54 +0100

    removed node_modules

--- server.ts
+++ server.ts
@@ -1,3 +1,5 @@
+import { createContext } from "vm";
+
 /* 
  * Setting Up Environment
  */
@@ -18,50 +20,52 @@ let wordPerRound: number = 2;
 //        RegEx um /joinN zu finden
 
 bot.command('start', (ctx) => {
-    for (let game of gameList) {
-        if (game.host.id == ctx.from.id) {
-            ctx.reply('Sie haben bereits ein Spiel gestartet, beenden Sie es vorher mit \'/stop\'');
-            return;
+    gameList.find(game => {
+        if(game.host.id == ctx.from.id) {
+            ctx.reply('Sie haben bereits Spiel ' + game.id + ' gestartet, beenden Sie es vorher mit \'/stop\'');
         }
-    }
-    let game = new Game(ctx.from);
-    gameList.push(game);
-    game.playerList.push(new Player(ctx.from));
-    ctx.reply('Dein Spiel wurde gestartet mit der ID: ' + game.id + '\nUm das Spiel zu starten tippe \'/begin\',\nUm dem Spiel beizutreten schreibe im Gruppen chat oder Privat \'/join ' + game.id + '\'');
+        else {
+            gameList.push(new Game(ctx.from));
+            game.addPlayer(game.host); 
+            ctx.reply('Dein Spiel wurde gestartet mit der ID: ' + game.id 
+            + '\nUm das Spiel zu starten tippe \'/begin\',\nUm dem Spiel beizutreten schreibe im Gruppen chat oder Privat \'/join ' + game.id + '\'');
+        }
+    })
 });
 
 bot.command('help', (ctx) => {
     ctx.reply('Um ein Spiel zu starten \'/start\' eingeben,\nUm dem Spiel als Spieler beizutreten benutzt man \'/join {SpielID}\'\nNachdem jeder beigetreten ist und eine Nachricht vom Bot bekommen hat kann man das Spiel mit \'/start\' Starten, dann sammelt der Bot die Worte\nWenn die richtige Anzahl an Worten gesammelt wurde kann man mit \'/begin\' starten.\nNach jeder Runde muss man mit \'/begin\' das Spiel starten, jede Runde wird mit \'next\' beendet. Diese Befehle kann nur der Host, also die Person die das Spiel gestartet hat benutzen, falls das nicht gut geht, kann man das noch aendern. Falls Einstellungen gewuenscht werden, kann das auch noch getan werden, wie Viele Worte pro Runde, wie viele Spielrunden, scoring etc.');
 });
 
+bot.command('show', (ctx) => {
+    for(let game of gameList) {
+        console.log("Running Game " + game.id);
+    }
+})
 
 //Join a game	    
 bot.command('join', (ctx) => {
-    let args = ctx.message.text.split(' ');
-    for (let game of gameList) {
-        if (game.id == args[1]) {
-            for (let player of game.playerList) {
-                if (player.id == ctx.from.id) {
-                    ctx.reply('Sie sind bereits Teil des Spiels.');
-                    return;
-                }
-            }
+    let pattern = new RegExp('^\/join(\d*)$');
+    let match = pattern.exec(ctx.message.text);
 
-            game.playerList.push(new Player(ctx.from));
-            bot.telegram.sendMessage(ctx.from.id, "Du bist Spiel " + game.id + " beigetreten.");
-            return;
+    gameList.find(game => {
+        if(game.id == parseInt(match[1])) {
+            game.playerList.find(player => {
+                if(player.id == ctx.from.id) {
+                    ctx.reply('Sie sind bereits Teil des Spiels');
+                }
+                else {
+                    game.addPlayer(new Player(ctx.from))
+                }
+            })
+        } else {
+            ctx.reply("Spiel wurde nicht gefunden")
         }
-    }
-    ctx.reply("Spiel wurde nicht gefunden.");
+    })
 });
 
 bot.command('begin', (ctx) => {
-    for (let game of gameList) {
-        if (game.host.id == ctx.from.id) {
-	    game.start();
-            return;
-        }
-    }
+    gameList.find(game => game.host.id == ctx.from.id).startGame();
 });
 
 bot.command('die', (ctx) => {
@@ -69,72 +73,40 @@ bot.command('die', (ctx) => {
 });
 
 bot.command('next', (ctx) => {
-    for (let game of gameList) {
-        if (game.host.id == ctx.from.id) {
-            game.nextRound();
-            return;
-        }
-    }
+    gameList.find(game => game.host.id == ctx.from.id).nextRound();
 });
 
 bot.command('stop', (ctx) => {
-    for (let game of gameList) {
-        if (game.host.id == ctx.from.id) {
-            ctx.reply("Spiel beendet. Ich hoffe ihr hattet Spass");
-            game.broadcast("Spiel " + game.id + " wurde beendet.");
-            gameList = gameList.filter((game) => game.host.id !== ctx.from.id);
-            return;
-        }
-    }
+    gameList.find(game => game.host.id == ctx.from.id).stopGame();
+    ctx.reply("Spiel beendet. Ich hoffe ihr hattet Spass");
 });
 
 bot.on('message', (ctx) => {
-    for (let game of gameList) {
-        if (game.gathering) {
-            for (let player of game.playerList) {
-                if (player.id == ctx.from.id) {
-                    if (player.wordList.length < (Math.floor(game.round / 4)+1) * wordPerRound) {
-                        player.wordList.push(ctx.message.text);
-                        game.wordList.push(ctx.message.text);
-                    }
-                    else {
-                        ctx.reply("Das Spiel braucht momentan keine neuen Worte.");
-                    }
-                }
-            }
-            if (game.wordList.length ==
-                (game.playerList.length * wordPerRound * (Math.floor(game.round / 4)+1)) ) {
-		console.log("Gathered " + game.wordList.length + " Words starting next round: " + (game.round));
-                if (game.round % 5 == 0 && game.wordList.length != 0) {
-                    game.nextRound();
-                }
-            }
-        }
-    }
+    gameList.find(game => {
+        if(game.addWord(ctx.from.id, ctx.message.text) == false) {
+            ctx.reply('Das Spiel braucht keine weiteren Worte mehr.')
+        }      
+    })
 });
 
 bot.command('save', (ctx) => {
-    let savestate: string;
     for(let game of gameList) {
-        for(let word of game.wordList) {
-            savestate += word;
-        }
+        ctx.reply(game.wordList);
     }
-    ctx.reply(savestate);
 });
 
 bot.command('leave', (ctx) => {
-    for(let game of gameList) {
-        for(let player of game.playerList) {
-            if(player.id == ctx.from.id) {
-                //game.removePlayer();
-            }
-        }
-    }
-})
-
+    gameList.find(game => game.playerList.find(player => {
+        player.id == ctx.from.id; 
+        game.removePlayer(player);
+    }))
+});
 
 bot.launch();
+
+function findGame(game: Game) {
+   // for()
+}
 
 class Game {
     id: number;
@@ -160,6 +132,28 @@ class Game {
             this.pickWord();
         }
     }
+    
+    addWord(id, word:string) :Boolean {
+        this.playerList.find(player => {
+            if(player.id == id) {
+                if (player.wordList.length < (Math.floor(this.round / 4)+1) * wordPerRound) {
+                    player.wordList.push(word);
+                    this.wordList.push(word);
+                }
+                else {
+                    return false;
+                }
+                if (this.wordList.length ==
+                    (this.playerList.length * wordPerRound * (Math.floor(this.round / 4)+1)) ) {
+                    console.log("Gathered " + this.wordList.length + " Words starting next round: " + (this.round));
+                    if (this.round % 5 == 0 && this.wordList.length != 0) {
+                        this.nextRound();
+                    }
+                }
+            }
+        })
+        return true;
+    }
 
     pickWord() {
         let pickableWords: string[] = this.wordList;
@@ -184,18 +178,31 @@ class Game {
 
     gatherWords() {
         console.log(this.wordList);
-	this.gathering = true;
-	this.broadcast("Bitte schreib mir 2 Worte deiner Wahl.");
+	    this.gathering = true;
+	    this.broadcast("Bitte schreib mir 2 Worte deiner Wahl.");
     }
     
-    start() {
-	console.log("Started game with " + this.playerList.length + " Players");
-	this.ongoing = true;
-	this.gatherWords();
+    startGame() {
+	    console.log("Started game with " + this.playerList.length + " Players");
+	    this.ongoing = true;
+	    this.gatherWords();
+    }
+
+    stopGame() {
+        console.log("Game " + this.id + " ended.");
+        this.broadcast("Spiel " + this.id + " wurde beendet.");
+        gameList = gameList.filter((game) => game != this);
+        return;
     }
 
     removePlayer(player) {
-      //  this.playerList = this.playerList.filter(() => player !=)
+        console.log('Player: ' + player.first_name + ' has left the game.')
+        this.playerList = this.playerList.filter((user) => user != player)
+    }
+
+    addPlayer(player) {
+        console.log('PLayer: ' + player.first_name + ' has joined the game.');
+        this.playerList = this.playerList.filter((user) => user != player);
     }
 
 }
