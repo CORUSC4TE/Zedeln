79656b06c8b0b21e92a5e54a87d60969ab2027a2
Noah Stewards <noah.stuertz@gmail.com>
2019-11-24 17:55:49 +0100

    fixes

--- server.ts
+++ server.ts
@@ -70,7 +70,8 @@ bot.command('join', (ctx) => {
 });
 
 bot.command('begin', (ctx) => {
-    gameList.find(game => game.host.id == ctx.from.id).startGame();
+    let game = gameList.find(game => game.host.id == ctx.from.id)
+    if(game) game.startGame();
 });
 
 bot.command('die', (ctx) => {
@@ -78,11 +79,13 @@ bot.command('die', (ctx) => {
 });
 
 bot.command('next', (ctx) => {
-    gameList.find(game => game.host.id == ctx.from.id).nextRound();
+    let game = gameList.find(game => game.host.id == ctx.from.id)
+    if (game) game.nextRound();
 });
 
 bot.command('stop', (ctx) => {
-    gameList.find(game => game.host.id == ctx.from.id).stopGame();
+    let game = gameList.find(game => game.host.id == ctx.from.id)
+    if (game) game.stopGame();
     ctx.reply("Spiel beendet. Ich hoffe ihr hattet Spass");
 });
 
@@ -104,9 +107,10 @@ bot.command('save', (ctx) => {
 });
 
 bot.command('leave', (ctx) => {
-    gameList.find(game => {
+    let game = gameList.find(game => {
         return game.playerList.some(player => player.id == ctx.from.id)
-    }).removePlayer(ctx.from)
+    })
+    if (game) game.removePlayer(ctx.from)
 });
 
 bot.launch();
